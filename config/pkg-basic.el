a842d52789fb60f564db41d93c109014831138f3
Lenic <Lenic@live.cn>
2020-04-15 09:55:23 +0800

    更新自动切换主题设置
    - 设置没有 GUI 时只使用一种主题不设置计时器
    - 修复启动时设置 previous-theme 时的异常
    
    Signed-off-by: Lenic <Lenic@live.cn>

--- config/pkg-basic.el
+++ config/pkg-basic.el
@@ -127,9 +127,10 @@
 (global-set-key (kbd "C-c C-p") 'copy-buffer-path)
 
 ;; 自动切换编辑器主题
+;; 控制台下只使用一种主题不设置定时器
 (if (equal nil (getenv "DISPLAY"))
     (load-theme 'spacemacs-dark t)
-  (setq day-theme 'solarized-light)
+  (setq day-theme 'spacemacs-light)
   (setq dark-theme 'spacemacs-dark)
   (setq previous-theme-name "")
   (setq previous-theme nil)
@@ -137,15 +138,14 @@
     (setq hour (string-to-number (substring (current-time-string) 11 13)))
     (setq current-theme nil)
     (if (member hour (number-sequence 6 16))
-        ((setq previous-theme dark-theme)
-         (setq current-theme day-theme))
-      (setq previous-theme day-theme)
+        (setq current-theme day-theme)
       (setq current-theme dark-theme))
     (setq current-theme-name (symbol-name current-theme))
     (unless (string= previous-theme-name current-theme-name)
       (setq previous-theme-name current-theme-name)
       (unless (equal nil previous-theme)
-        (disable-theme previous-theme))
+        (disable-theme previous-theme)
+        (setq previous-theme nil))
       (load-theme current-theme t)
       (setq previous-theme current-theme)))
   ;; 每 10 分钟运行一次检查
