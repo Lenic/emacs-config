1cc6bca17566235f5134b86b92e0a04796f3dc38
Lenic <Lenic@live.cn>
2020-04-15 10:41:12 +0800

    拆分 Basic 配置为两个：Basic、Global
    Signed-off-by: Lenic <Lenic@live.cn>

--- config/pkg-basic.el
+++ config/pkg-basic.el
@@ -2,6 +2,12 @@
 ;; (set-default-font "Ubuntu Mono 14")
 (set-default-font "文泉驿等宽正黑 14")
 
+;; 设置自动加载已修改文件
+(global-auto-revert-mode t)
+
+;; 高亮当前行
+(global-hl-line-mode t)
+
 ;; 编码设置
 (prefer-coding-system 'utf-8)
 (set-default-coding-systems 'utf-8)
@@ -75,81 +81,10 @@
 ;; 设置光标样式
 (setq-default cursor-type 'box)
 
-;; 设置 Ace-Jump
-(global-set-key (kbd "C-c j") 'ace-jump-word-mode)
-
-;; swiper 配置
-;; (global-set-key (kbd "C-s") 'swiper-isearch)
-
 ;; 设置每次前进或者后退搜索后将目标位置放置在屏幕垂直居中
 (defadvice isearch-repeat-forward (after isearch-repeat-forward-recenter activate) (recenter))
 (defadvice isearch-repeat-backward (after isearch-repeat-backward-recenter activate) (recenter))
-
 (ad-activate 'isearch-repeat-forward)
 (ad-activate 'isearch-repeat-backward)
-
-;; 替换命令执行
-(global-set-key (kbd "M-x") 'counsel-M-x)
-
-;; 替换 Buffer 界面
-(global-set-key (kbd "C-x C-b") 'counsel-ibuffer)
-
-;; 替换打开文件
-(global-set-key (kbd "C-x C-f") 'counsel-find-file)
-
-;; 设置 AG 项目文件查找
-(global-set-key (kbd "C-c k") 'counsel-ag)
-
-;; 设置 Undo-Tree
-(global-undo-tree-mode)
-(setq undo-tree-visualizer-timestamps t)
-(setq undo-tree-visualizer-diff t)
-
-;; 设置全局 Git 状态显示
-(global-git-gutter+-mode)
-
-;; 在项目中快速查找文件
-(setq project-enable-caching t)
-(setq projectile-completion-system 'ivy)
-(setq projectile-indexing-method 'alien)
-(global-set-key (kbd "C-c p") 'projectile-find-file)
-
-;; 拷贝当前 Buffer 到剪切板
-(defun copy-buffer-path ()
-  (interactive)
-  (if (equal buffer-file-name nil)
-      (message "没有文件名")
-    (setq path (vc-find-root buffer-file-name ".git"))
-    (if (equal path nil)
-	(kill-new (message buffer-file-name))
-      (setq absolutePath (expand-file-name path))
-      (setq targetPath (substring buffer-file-name (length absolutePath)))
-      (kill-new (message targetPath)))))
-(global-set-key (kbd "C-c C-p") 'copy-buffer-path)
-
-;; 自动切换编辑器主题
-;; 控制台下只使用一种主题不设置定时器
-(if (equal nil (getenv "DISPLAY"))
-    (load-theme 'spacemacs-dark t)
-  (setq day-theme 'spacemacs-light)
-  (setq dark-theme 'spacemacs-dark)
-  (setq previous-theme-name "")
-  (setq previous-theme nil)
-  (defun synchronize-theme ()
-    (setq hour (string-to-number (substring (current-time-string) 11 13)))
-    (setq current-theme nil)
-    (if (member hour (number-sequence 6 16))
-        (setq current-theme day-theme)
-      (setq current-theme dark-theme))
-    (setq current-theme-name (symbol-name current-theme))
-    (unless (string= previous-theme-name current-theme-name)
-      (setq previous-theme-name current-theme-name)
-      (unless (equal nil previous-theme)
-        (disable-theme previous-theme)
-        (setq previous-theme nil))
-      (load-theme current-theme t)
-      (setq previous-theme current-theme)))
-  ;; 每 10 分钟运行一次检查
-  (run-with-timer 0 600 'synchronize-theme))
 
 (provide 'pkg-basic)
