371c36ad5d71d5f69c852b483ff282560437ef2e
Lenic <Lenic@live.cn>
2020-04-16 11:23:37 +0800

    更新 Org 模式的相关配置
    Signed-off-by: Lenic <Lenic@live.cn>

--- config/pkg-org.el
+++ config/pkg-org.el
@@ -1,23 +1,27 @@
 ;; 设置全局快捷键打开默认的任务管理
 (global-set-key (kbd "<f2>") (lambda () (interactive) (find-file "~/task/mj.org")))
 
-;; 设置 TODO 任务的几个状态
-(setq org-todo-keywords
-      '((sequence "TODO(t)" "START(s!)" "PAUSE(p@/!)" "|" "DONE(d!)")
-	(sequence "BUG(b)" "|" "FIXED(f!)")
-	(sequence "|" "CANCELED(c@/!)")))
-
-;; org 自动换行
-(add-hook 'org-mode-hook (lambda () (setq truncate-lines nil)))
-
-;; 设置 Bullets
-(add-hook 'org-mode-hook 'org-bullets-mode)
-
-;; 设置使用 indent 模式
-(add-hook 'org-mode-hook 'org-indent-mode)
-
 ;; 设置快速捕获
 (global-set-key (kbd "C-c c") 'org-capture)
+
+;; 设置 Agenda 模块快捷键
+(global-set-key "\C-ca" 'org-agenda)
+
+(add-hook 'org-mode-hook
+          (lambda ()
+            ;; org 自动换行
+            (setq truncate-lines nil)
+            ;; 设置 Bullets
+            (org-bullets-mode t)
+            ;; 设置使用 indent 模式
+            (org-indent-mode t)
+            ;; 设置 TODO 任务的几个状态
+            (setq org-todo-keywords
+                  '((sequence "TODO(t)" "START(s!)" "PAUSE(p@/!)" "|" "DONE(d!)")
+                    (sequence "BUG(b)" "|" "FIXED(f!)")
+                    (sequence "|" "CANCELED(c@/!)")))))
+
+;; 设置缺省的日志记录位置
 (setq org-default-notes-file "~/task/inbox.org")
 
 ;; 移除系统默认的 Capture 模版
@@ -51,8 +55,5 @@
 ;; 添加个人日记本
 (add-to-list 'org-capture-templates
              '("mj" "日记本" entry (file+datetree "~/task/me.journal.org") "* %U\n%?"))
-
-;; 设置 Agenda 模块快捷键
-(global-set-key "\C-ca" 'org-agenda)
 
 (provide 'pkg-org)
