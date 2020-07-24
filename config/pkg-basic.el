73f7c74dcaaa0a156c9e258efe40899238e8c8b6
Lenic <Lenic@live.cn>
2020-07-24 18:19:10 +0800

    移除默认字体的大小为 13 号
    - 移除 Treemacs 插件
    
    Signed-off-by: Lenic <Lenic@live.cn>

--- config/pkg-basic.el
+++ config/pkg-basic.el
@@ -6,7 +6,7 @@
               (tool-bar-lines . 0)
               ;; (font . "Ubuntu Mono 14")
               ;; (font . "文泉驿等宽正黑 14")
-              (font . "Noto Sans Mono CJK SC 14")
+              (font . "Noto Sans Mono CJK SC 13")
               (width . 140) ; chars
               (height . 30) ; lines
               (left . 0)
@@ -16,7 +16,7 @@
               (tool-bar-lines . 0)
               ;; (font . "Ubuntu Mono 14")
               ;; (font . "文泉驿等宽正黑 14")
-              (font . "Noto Sans Mono CJK SC 14")
+              (font . "Noto Sans Mono CJK SC 13")
               (width . 140)
               (height . 30)
               (left . 0)
