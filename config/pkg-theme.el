32006ccc384a0a1b114f10b44235873f80fec3fc
Lenic <Lenic@live.cn>
2020-08-05 17:36:11 +0800

    更新主题和镜像源设置
    Signed-off-by: Lenic <Lenic@live.cn>

--- config/pkg-theme.el
+++ config/pkg-theme.el
@@ -1,11 +1,11 @@
-(use-package gruvbox-theme)
+(use-package solarized-theme)
 (use-package spacemacs-theme
-  :after gruvbox-theme
+  :after (solarized-theme)
   :defer t
   :init
   ;; 自动切换编辑器主题
   ;; 控制台下只使用一种主题不设置定时器
-  (setq day-theme 'gruvbox-light-soft)
+  (setq day-theme 'solarized-light)
   (setq dark-theme 'spacemacs-dark)
   (setq previous-theme-name "")
   (setq previous-theme nil)
