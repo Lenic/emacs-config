8d4410d669f226262b6eda5c9def20ed8ca92234
Lenic <Lenic@live.cn>
2020-10-24 17:38:19 +0800

    前端开发全部统一使用 LSP 模式
    - 移除 Tide 插件：放弃自动导入功能
    
    Signed-off-by: Lenic <Lenic@live.cn>

--- config/pkg-web.el
+++ config/pkg-web.el
@@ -1,22 +1,6 @@
-(define-derived-mode web-vue-mode web-mode "WVue"
-  "A major mode derived from web-mode, for editing .vue files with LSP support.")
-(define-derived-mode web-react-mode web-mode "WReact"
-  "A major mode derived from web-mode, for editing .jsx files with LSP support.")
-
 ;; 设置保存后自动格式化代码
 (use-package prettier-js
   :hook ((css-mode web-mode typescript-mode js-mode json-mode) . prettier-js-mode))
-
-;; LSP 模式的 JS 自动完成配置
-(use-package tide
-  :hook
-  ((web-react-mode) .
-   (lambda ()
-     ;; Tide 安装
-     (tide-setup)
-     (tide-hl-identifier-mode t)
-     ;; 当 tsserver 服务没有启动时自动重新启动
-     (unless (tide-current-server) (tide-restart-server)))))
 
 ;; 快速编写 HTML 代码
 (use-package emmet-mode
@@ -25,6 +9,8 @@
 
 ;; 附加 Web 开发的各种插件
 (defun web-dev-attached ()
+  ;; 开启 LSP 模式自动完成
+  (lsp)
   ;; 设置关闭自动换行
   (setq truncate-lines t)
   ;; 开启显示行号
@@ -57,6 +43,7 @@
               (web-dev-attached))))
 
 (use-package web-mode
+  :mode ("\\.jsx?\\'" "\\.vue\\'")
   :init
   ;; 设置默认的缩进
   (setq web-mode-css-indent-offset 2
@@ -64,13 +51,7 @@
         web-mode-markup-indent-offset 2)
   (setq web-mode-enable-current-column-highlight t)
   :config
-  (add-to-list 'auto-mode-alist '("\\.vue\\'" . web-vue-mode))
-  (add-to-list 'auto-mode-alist '("\\.jsx?\\'" . web-react-mode))
-  (add-hook 'web-vue-mode-hook 'lsp)
-  ;; 设置语法高亮模式
-  (add-hook 'web-react-mode-hook '(lambda ()
-                                    (web-dev-attached)
-                                    (setq web-mode-content-types-alist '(("jsx" . "\\.js[x]?\\'"))))))
+  (add-hook 'web-mode-hook 'web-dev-attached))
 
 (use-package company-quickhelp
   :init
