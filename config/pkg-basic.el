dc94097497b5dc1785306a5c03a268599c185e2b
Lenic <Lenic@live.cn>
2021-01-05 14:37:56 +0800

    添加 M-h 删除光标前单词的按键绑定
    Signed-off-by: Lenic <Lenic@live.cn>

--- config/pkg-basic.el
+++ config/pkg-basic.el
@@ -160,6 +160,7 @@
 ;; 设置 Ctrl-h 为删除前一个字符
 (unbind-key "C-h")
 (global-set-key (kbd "C-h") 'delete-backward-char)
+(global-set-key (kbd "M-h") 'backward-kill-word)
 
 ;; 设置光标样式
 (setq-default cursor-type 'box)
