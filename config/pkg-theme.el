d82a97b0770fcbca751100a1764796fb17ea7a55
Lenic <Lenic@live.cn>
2021-07-14 20:54:26 +0800

    更新自动主题切换逻辑
    Signed-off-by: Lenic <Lenic@live.cn>

--- config/pkg-theme.el
+++ config/pkg-theme.el
@@ -9,92 +9,122 @@
     :init
     (setq powerline-default-separator 'slant)))
 
-;; 自动切换编辑器主题
+;; 设置日间主题和夜间主题
 (setq day-theme nil)
 (setq dark-theme nil)
-(setq previous-theme-name "")
-(setq previous-theme nil)
-(defun synchronize-theme ()
-  (interactive)
-  (setq hour (string-to-number (substring (current-time-string) 11 13)))
+
+;; 获取当前主题
+(defun get-current-theme ()
   (setq current-theme nil)
-  (if (member hour (number-sequence 6 17))
-      (setq current-theme day-theme)
-    (setq current-theme dark-theme))
-  (setq current-theme-name (symbol-name current-theme))
-  (unless (string= previous-theme-name current-theme-name)
-    (setq previous-theme-name current-theme-name)
-    (unless (equal nil previous-theme)
-      (disable-theme previous-theme))
-    ;; 更新主题
-    (load-theme current-theme t)
-    ;; 更改中文时光标的色值：延迟 1s 执行
-    (run-with-timer 1 nil (lambda ()
-                            (if (string= current-theme-name (symbol-name day-theme))
-                                (progn
-                                  (set-cursor-color "#666")
-                                  (setq pyim-indicator-cursor-color (list "purple" "#666")))
-                              (progn
-                                (set-cursor-color "#b2b2b2")
-                                (setq pyim-indicator-cursor-color (list "#ff72ff" "#b2b2b2"))))))    
-    ;; 重新编译 Spaceline
-    (if (spaceline-compile)
-        (spaceline-compile))
-    ;; 设置高亮列的背景色
-    ;; (set-face-attribute 'col-highlight nil :inherit 'hl-line :background)
-    (setq previous-theme current-theme)))
+  (if (= (length custom-enabled-themes) 0)
+      (setq current-theme (nth 0 custom-enabled-themes))))
+
+;; 切换编辑器主题
+(defun switch-theme (&optional target-theme)
+  ;; 没传参数时直接获取当前主题
+  (if (eq target-theme nil)
+      (setq target-theme (get-current-theme)))
+  ;; 只有设置了主题才能继续
+  (if (not (eq target-theme nil))
+      ;; 是明亮主题就切换为明亮主题；否则就反过来
+      (if (eq target-theme day-theme)
+          (progn
+            (message "day-theme")
+            (load-theme day-theme t)
+            (set-cursor-color "#666")
+            (setq pyim-indicator-cursor-color (list "purple" "#666")))
+        (progn
+          (message "dark-theme")
+          (load-theme dark-theme t)
+          (set-cursor-color "#b2b2b2")
+          (setq pyim-indicator-cursor-color (list "#ff72ff" "#b2b2b2"))))))
+
+;; 自动切换编辑器主题
+(defun synchronize-theme ()
+  ;; 获取是否处在白天
+  (setq is-in-day nil)
+  ;; 获取当前处于几点：24 小时制
+  (setq hour (string-to-number (substring (current-time-string) 11 13)))
+  ;; 判断当前是否处于白天：从早上 6 点开始到晚上过了 17 点
+  (setq is-in-day (member hour (number-sequence 6 17)))
+  ;; 获取当前主题
+  (setq current-theme (get-current-theme))
+  ;; 设置是否已经执行了主题切换
+  (setq theme-changed nil)
+  ;; 处于白天并且当前主题不是明亮主题时执行
+  (if (and (eq is-in-day t) (not (eq current-theme day-theme)))
+      (progn
+        (switch-theme day-theme)
+        (setq theme-changed t)))
+  ;; 处于晚上并且当前主题不是暗黑主题时执行
+  (if (and (eq is-in-day nil) (not (eq current-theme dark-theme)))
+      (progn
+        (switch-theme dark-theme)
+        (setq theme-changed t)))
+  (if theme-changed
+      (progn
+        ;; 设置高亮列的背景色
+        ;; (set-face-attribute 'col-highlight nil :inherit 'hl-line :background)
+        ;; 重新编译 Spaceline
+        (if (spaceline-compile)
+            (spaceline-compile)))))
+
+;; 当前主题检查循环是否正在运行
+(setq is-theme-running nil)
+;; 执行主题切换检查
+(defun run-theme()
+  (if (eq is-theme-running nil)
+      (progn
+        ;; 设置自动主题更换已经运行
+        (setq is-theme-running t)
+        ;; 加载 spaceline
+        (load-spaceline)
+        ;; 设置启动后全屏
+        (if (display-graphic-p)
+            (run-with-timer 1 nil 'toggle-frame-fullscreen))
+        ;; GUI 模式下才自动运行主题切换：每 10 分钟运行一次检查
+        (if (display-graphic-p)
+            (setq token-of-interval (run-with-timer 0 600 'synchronize-theme))
+          ;; Terminal 下永久使用 dark-theme 主题
+          (load-theme dark-theme t)))))
 
 (use-package spacemacs-theme
   :defer t
   :init
-  (setq is-theme-running nil)
+  ;; 设置明亮主题
   (setq day-theme 'spacemacs-light)
+  ;; 设置暗黑主题
   (setq dark-theme 'spacemacs-dark)
   ;; 当前活跃 frame 的数量，默认的数量是 0
   (setq current-frame-count 0)
   ;; 当前正在执行的 interval token，默认值为 nil，开始运行主题切换检查后置为 t
   (setq token-of-interval nil)
-  ;; 执行主题切换检查
-  (defun run-theme()
-    ;; 加载 spaceline
-    (load-spaceline)
-    ;; 设置启动后全屏
-    (if (display-graphic-p)
-        (run-with-timer 1 nil 'toggle-frame-fullscreen))
-    ;; 设置自动主题更换已经运行
-    (setq is-theme-running t)
-    ;; GUI 模式下才自动运行主题切换：每 10 分钟运行一次检查
-    (if (display-graphic-p)
-        (setq token-of-interval (run-with-timer 0 600 'synchronize-theme))
-      ;; Terminal 下永久使用 dark-theme 主题
-      (load-theme dark-theme t)))
   ;; 启动时不是 daemon 模式就执行主题设置
   (if (eq (daemonp) nil)
       (progn
         ;; 初始不是后台的方式启动，设置当前活跃的 frame 数量 +1
         (setq current-frame-count (+ current-frame-count 1))
+        ;; 执行设置主题功能
         (run-theme)))
   (add-hook 'after-make-frame-functions
             (lambda (new-frame)
               (select-frame new-frame)
               ;; 每次新建 frame 设置当前活跃的 frame 数量 +1
               (setq current-frame-count (+ current-frame-count 1))
-              (if (eq is-theme-running nil)
-                  (run-theme))))
+              ;; 执行设置主题功能
+              (run-theme)))
   (add-hook 'after-delete-frame-functions
             (lambda (deleted-frame)
               ;; 每次销毁 frame 设置当前活跃的 frame 数量 -1
               (setq current-frame-count (- current-frame-count 1))
               ;; 销毁了全部的活跃 frame 后停止主题切换检查，同时复位 is-theme-running
-              (if (eq current-frame-count 0)
+              (if (= current-frame-count 0)
                   (progn
                     (setq is-theme-running nil)
                     (cancel-timer token-of-interval))))))
 
 ;; 输入法设置
-(use-package popup)
 (use-package pyim
-  :after popup
   :demand t
   :config
   ;; 激活 basedict 拼音词库
