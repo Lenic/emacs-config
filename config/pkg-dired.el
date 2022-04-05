e2c5848ad553f49ea00f29b7394d6ed03af4382c
Lenic <Lenic@live.cn>
2022-04-05 23:21:53 +0800

    更新 Dired 模式的 Buffer 数量限制设置
    - 根据 28.1 新添加的变量更新
    
    Signed-off-by: Lenic <Lenic@live.cn>

--- config/pkg-dired.el
+++ config/pkg-dired.el
@@ -1,16 +1,14 @@
 (use-package dired
   :defer 10
   :ensure nil
+  :custom
+  (dired-kill-when-opening-new-dired-buffer t)
   :config
   ;; 在 macOS 上，ls 不支持 --dired 选项，而在 Linux 上则受支持
   (when (string= system-type "darwin")
     (setq dired-use-ls-dired nil))
   ;; 设置 Dired 模式显示方式：增加文件大小的可读性
   (setq dired-listing-switches "-alh")
-  ;; 设置使用单个 Buffer 打开文件夹
-  (put 'dired-find-alternate-file 'disabled nil)
-  ;; 设置回车直接在当前 Buffer 打开文件夹
-  (define-key dired-mode-map (kbd "RET") 'dired-find-alternate-file)
   ;; 设置 ^ 直接回到上一级文件夹
   (define-key dired-mode-map (kbd "^") (lambda () (interactive) (find-alternate-file ".."))))
 
