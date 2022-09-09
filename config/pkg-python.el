c197974b92d0d91b8b5e44e12d3faa2b8ac44eb8
Lenic <Lenic@live.cn>
2022-09-09 12:32:45 +0800

    完成 Python 的 LSP 自动提示功能
    - 包含了光标处的类型提示
    
    Signed-off-by: Lenic <Lenic@live.cn>

--- config/pkg-python.el
+++ config/pkg-python.el
@@ -1,26 +1,18 @@
 ;; Python 开发主模式
 (use-package python-mode
-  :defer 3
-  :config
-  (add-hook 'python-mode-hook
-            (lambda ()
-              ;; 开启 LSP 模式自动完成
-              (lsp)
-              (setq lsp-pylsp-plugins-flake8-enabled nil)
-              ;; 设置 pylsp 文件搜索目录
-              (setq lsp-clients-pylsp-library-directories "/Users/leixuewei/Library/Python/3.8/bin")
-              ;; 设置自动缩进的宽度
-              (setq-default indent-tabs-mode t)
-              (setq-default tab-width 4)
-              (setq-default py-indent-tabs-mode t)
-              ;; (setq-default flycheck-disabled-checkers '(python-flake8 python-pylint python-pycompile python-pyright python-mypy))
-              (setq-default flycheck-disabled-checkers '(lsp))
-              (add-to-list 'write-file-functions 'delete-trailing-whitespace))))
+  :defer 3)
+
+;; LSP 自动完成服务端
+(use-package lsp-pyright
+  :after python-mode
+  :hook (python-mode . (lambda ()
+                         (require 'lsp-pyright)
+                         (lsp))))  ; or lsp-deferred
 
 ;; Python 保存自动格式化工具
 (use-package python-black
   :demand t
-  :after python
+  :after python-mode
   :hook (python-mode . python-black-on-save-mode))
 
 (provide 'pkg-python)
