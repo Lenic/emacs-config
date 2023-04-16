453c0951129b6e195f72191afaa754ce04a2e0f2
Lenic Rayleigh <Lenic@live.cn>
2023-04-16 22:26:20 +0800

    移除不再使用 OrgRoam 配置
    Signed-off-by: Lenic Rayleigh <Lenic@live.cn>

--- config/pkg-org.el
+++ config/pkg-org.el
@@ -46,35 +46,4 @@
 (add-to-list 'org-capture-templates
              '("m" "个人生活" entry (file+headline "~/task/me.inbox.org" "Tasks") "* TODO %?\n%U\n%a"))
 
-;; ;; 设置 Roam 模式
-;; (use-package company-org-roam)
-;; (use-package org-roam
-;;   :after company-org-roam
-;;   :hook
-;;   (after-init . org-roam-mode)
-;;   :custom
-;;   (org-roam-directory "~/roam/")
-;;   :bind (:map org-roam-mode-map
-;;               (("C-c n l" . org-roam)
-;;                ("C-c n f" . org-roam-find-file)
-;;                ("C-c n g" . org-roam-graph))
-;;               :map org-mode-map
-;;               (("C-c n i" . org-roam-insert))
-;;               (("C-c n I" . org-roam-insert-immediate))))
-;; ;; 设置 Org-Roam 的 Server 模式
-;; (use-package org-roam-server
-;;   :ensure t
-;;   :config
-;;   (setq org-roam-server-host "127.0.0.1"
-;;         org-roam-server-port 10086
-;;         org-roam-server-authenticate nil
-;;         org-roam-server-export-inline-images t
-;;         org-roam-server-serve-files nil
-;;         org-roam-server-served-file-extensions '("pdf" "mp4" "ogv")
-;;         org-roam-server-network-poll t
-;;         org-roam-server-network-arrows nil
-;;         org-roam-server-network-label-truncate t
-;;         org-roam-server-network-label-truncate-length 60
-;;         org-roam-server-network-label-wrap-length 20))
-
 (provide 'pkg-org)
