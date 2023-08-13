4d8620bf01672252c5643bb774b7d755458a4f3a
Lenic Rayleigh <Lenic@live.cn>
2023-08-13 15:58:43 +0800

    更新 tree sitter 配置和 expand-region 对于 ts 后缀 mode 的适配
    Signed-off-by: Lenic Rayleigh <Lenic@live.cn>

--- config/pkg-dev.el
+++ config/pkg-dev.el
@@ -1,30 +1,11 @@
 ;; 添加 treesit 语言配置
-(setq treesit-font-lock-level 4
-      treesit-language-source-alist
-      '((bash "https://github.com/tree-sitter/tree-sitter-bash")
-        (cmake "https://github.com/uyha/tree-sitter-cmake")
-        (css "https://github.com/tree-sitter/tree-sitter-css")
-        (elisp "https://github.com/Wilfred/tree-sitter-elisp")
-        (go "https://github.com/tree-sitter/tree-sitter-go")
-        (html "https://github.com/tree-sitter/tree-sitter-html")
-        (javascript "https://github.com/tree-sitter/tree-sitter-javascript" "master" "src")
-        (json "https://github.com/tree-sitter/tree-sitter-json")
-        (make "https://github.com/alemuller/tree-sitter-make")
-        (markdown "https://github.com/ikatyang/tree-sitter-markdown")
-        (python "https://github.com/tree-sitter/tree-sitter-python")
-        (toml "https://github.com/tree-sitter/tree-sitter-toml")
-        (tsx "https://github.com/tree-sitter/tree-sitter-typescript" "master" "tsx/src")
-        (typescript "https://github.com/tree-sitter/tree-sitter-typescript" "master" "typescript/src")
-        (yaml "https://github.com/ikatyang/tree-sitter-yaml")
-        (vue "https://github.com/ikatyang/tree-sitter-vue")))
+(use-package treesit-auto
+  :demand t
+  :config
+  (setq treesit-auto-install 'prompt)
+  (global-treesit-auto-mode))
 
-(setq major-mode-remap-alist
-      '((js-mode . js-ts-mode)
-        (typescript-mode . tsx-ts-mode)
-        (typescript-ts-mode . tsx-ts-mode)
-        (json-mode . json-ts-mode)
-        (css-mode . css-ts-mode)
-        (python-mode . python-ts-mode)))
+(setq treesit-font-lock-level 4)
 
 ;; 项目列表选择工具
 (use-package projectile
@@ -152,22 +133,22 @@
   :commands (er/expand-region er/mark-word)
   :bind ("C-o" . er/expand-region)
   :config
-  (defun tree-sitter-mark-bigger-node ()
-    (interactive)
-    (let* ((root (tsc-root-node tree-sitter-tree))
-           (node (tsc-get-descendant-for-position-range root (region-beginning) (region-end)))
-           (node-start (tsc-node-start-position node))
-           (node-end (tsc-node-end-position node)))
-      ;; Node fits the region exactly. Try its parent node instead.
-      (when (and (= (region-beginning) node-start) (= (region-end) node-end))
-        (when-let ((node (tsc-get-parent node)))
-          (setq node-start (tsc-node-start-position node)
-                node-end (tsc-node-end-position node))))
-      (set-mark node-end)
-      (goto-char node-start)))
+  (when (treesit-available-p)
+    (defun my/treesit-mark-bigger-node ()
+      "https://emacs-china.org/t/treesit-expand-region-el/23406"
+      (let* ((root (treesit-buffer-root-node))
+             (node (treesit-node-descendant-for-range root (region-beginning) (region-end)))
+             (node-start (treesit-node-start node))
+             (node-end (treesit-node-end node)))
+        ;; Node fits the region exactly. Try its parent node instead.
+        (when (and (= (region-beginning) node-start) (= (region-end) node-end))
+          (when-let ((node (treesit-node-parent node)))
+            (setq node-start (treesit-node-start node)
+                  node-end (treesit-node-end node))))
+        (set-mark node-end)
+        (goto-char node-start)))
 
-  (setq er/try-expand-list (append er/try-expand-list
-                                   '(tree-sitter-mark-bigger-node))))
+    (add-to-list 'er/try-expand-list 'my/treesit-mark-bigger-node)))
 
 
 
